c19baff259e4fddb7ee65451bfc75509aa11c972
Alex291 <underrail291@gmail.com>
2026-01-06 16:42:31 +0000

    Second commit

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import "fmt"
 
-func main(){
-	for count := range 20{
-		fmt.Println("Hello world", count)
-	}
+func main() {
+	fmt.Println("func main was called")
+	printPersonalData()
+	acceptor("Alex", "Timofeev", 54)
+	fmt.Println("func main has ended")
+}
+
+func printPersonalData() {
+	fmt.Println("func printPersonalData was called")
+	firstName := "Alex"
+	secondName := "Timofeev"
+	age := 54
+	fmt.Println(firstName)
+	fmt.Println(secondName)
+	fmt.Println(age)
+	fmt.Println("func printPersonalData has ended")
+}
+
+func acceptor(
+	firstName string,
+	secondName string,
+	age int,
+) {
+	fmt.Println("func acceptor was called")
+	fmt.Println(firstName, secondName, age)
+	fmt.Println("func acceptor hs ended")
+
 }
