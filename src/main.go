cbaafb733b869aa334548c020bf9fb87cd030b1e
Alex291 <underrail291@gmail.com>
2026-01-28 18:54:21 +0000

    my lesson with Geminy

--- src/main.go
+++ src/main.go
@@ -1,14 +1,38 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Enter your age")
-	var age int
-	itemsScanned, err := fmt.Scan(age)
-	fmt.Println("item scanned", itemsScanned)
-	fmt.Println(err.Error())
-	fmt.Println("Your age", age)
+
+	var name string
+
+	name = "This is test, This is test, v, This is test"
+
+	fmt.Println(name)
+
+	var byteArray [10]byte
+
+	fmt.Println(byteArray)
+
+	var intArray [3]int
+
+	intArray = [3]int{299, 10, 50}
+
+	fmt.Println(intArray, "length:", len(intArray))
+
+	fmt.Println("first number is", intArray[2])
+
+	boolArray := [1]bool{true}
+
+	fmt.Println(boolArray)
+
+	number := [3]int{10, 20, 30}
+
+	fmt.Println(number, "length:", len(number))
+
+	number[1] = 7
+
+	for i, v := range number {
+		fmt.Println("На полке №", i, "лежит:", v)
+	}
 }
