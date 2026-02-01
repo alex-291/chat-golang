3ef70ada8e0a7a6470e9034e32d0f9b20ece0447
Alex291 <underrail291@gmail.com>
2026-02-01 17:30:58 +0000

    using slices example with bus

--- src/main.go
+++ src/main.go
@@ -30,4 +30,30 @@ func main() {
 	numbersSlice[0] = 46
 	fmt.Println(numbersSlice, "length:", len(numbersSlice), "capacity:", cap(numbersSlice))
 
+	word := "New"
+	fmt.Println(word)
+
+	// make([]тип, сколько_пассажиров, сколько_кресел)
+	bus := make([]int, 0, 5)
+
+	fmt.Println("Сколько пассажиров (len):", len(bus))
+	fmt.Println("Сколько всего кресел (cap):", cap(bus))
+
+	bus = append(bus, 10) // посадили одного
+	bus = append(bus, 20) // посадили второго
+
+	fmt.Println("Пассажиров:", len(bus)) // теперь 2
+	fmt.Println("Кресел:", cap(bus))     // всё еще 5s
+
+	// Допустим, мы заполнили все 5 мест
+	bus = append(bus, 30, 40, 50)
+	fmt.Println("Заполнено:", len(bus), "из", cap(bus)) // 5 из 5
+
+	// Пытаемся посадить 6-го!
+	bus = append(bus, 60)
+
+	fmt.Println("После перебора:")
+	fmt.Println("Новый Len:", len(bus)) // станет 6
+	fmt.Println("Новый Cap:", cap(bus)) // станет 10! (удвоился)
+
 }
